19295320c12475d033253631b30f4c142f53585e
tomi <tomiwest@hotmail.com>
2023-01-31 14:15:16 +0200

    Task 13.1. && Task 13.2.

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -10,31 +10,37 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
   },
 })
-class Note extends Model {}
+class Blog extends Model {}
 
-Note.init(
+Blog.init(
   {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
     },
-    content: {
+    author: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    url: {
       type: DataTypes.TEXT,
       allowNull: false,
     },
-    important: {
-      type: DataTypes.BOOLEAN,
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
-    date: {
-      type: DataTypes.DATE,
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
     },
   },
   {
     sequelize,
     underscored: true,
     timestamps: false,
-    modelName: 'note',
+    modelName: 'blog',
   }
 )
 
