95a7781f4305f70893a575f302c917293f762724
tomi <tomiwest@hotmail.com>
2023-02-01 15:28:35 +0200

    token auth when adding blog

--- blog-backend/controllers/blogs.js
+++ blog-backend/controllers/blogs.js
@@ -1,6 +1,22 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
+const { Blog, User } = require('../models')
 
-const { Blog } = require('../models')
+const tokenExtractor = (req, res, next) => {
+  console.log(req.body)
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
@@ -26,10 +42,14 @@ router.put('/:id', blogFinder, async (req, res) => {
   res.json(likesUpdatedBlog)
 })
 
-router.post('/', async (req, res) => {
-  const blog = await Blog.create(req.body)
-
-  return res.json(blog)
+router.post('/', tokenExtractor, async (req, res) => {
+  try {
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.create({ ...req.body, userId: user.id })
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
 })
 
 router.delete('/:id', blogFinder, async (req, res) => {
