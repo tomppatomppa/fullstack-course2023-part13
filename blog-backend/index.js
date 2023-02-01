53fb47afbb25962989dbfde8eee79cbf1ceaa56e
tomi <tomiwest@hotmail.com>
2023-02-01 15:13:05 +0200

    Exercise 13.10.

--- blog-backend/index.js
+++ blog-backend/index.js
@@ -8,11 +8,13 @@ require('express-async-errors')
 
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 const start = async () => {
   await connectToDatabase()
--- blog-backend/models/index.js
+++ blog-backend/models/index.js
@@ -2,7 +2,6 @@ const Blog = require('./blog')
 const User = require('./user')
 
 Blog.sync()
-
 User.sync()
 
 module.exports = {
