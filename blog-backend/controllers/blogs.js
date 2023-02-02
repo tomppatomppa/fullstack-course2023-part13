44ed991607e103abfc442ded1abcd7abeebdcbfe
tomi <tomiwest@hotmail.com>
2023-02-02 11:36:27 +0200

    Exercise 13.14.

--- blog-backend/controllers/blogs.js
+++ blog-backend/controllers/blogs.js
@@ -30,11 +30,14 @@ const blogFinder = async (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
-  const where = {}
+  let where = {}
 
   if (req.query.search) {
-    where.title = {
-      [Op.substring]: req.query.search,
+    where = {
+      [Op.or]: [
+        { title: { [Op.substring]: req.query.search } },
+        { author: { [Op.substring]: req.query.search } },
+      ],
     }
   }
 
